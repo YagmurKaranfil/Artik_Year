398e6ac8f6138e5f49e3db9fb3c2d16c2291488e
YagmurKaranfil <59535304+YagmurKaranfil@users.noreply.github.com>
2022-11-15 17:01:44 +0300

    Artik_Year

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int year;
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Yıl Giriniz : ");
+        year=input.nextInt();
+
+        if ((year % 100 == 0)) {
+            if (year % 400 == 0) {
+                System.out.print(year + " bir artık yıldır");
+            } else {
+                System.out.print(year + " bir artık yıl değildir");
+            }
+        } else if (year % 4 == 0) {
+            System.out.print(year + " bir artık yıldır");
+        } else {
+            System.out.print(year + " bir artık yıl değildir");
+        }
+
+    }
+}
